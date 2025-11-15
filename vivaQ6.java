78968e18953063a33b4d98d353c840d965afc14c
JunBin05 <limjunbin05@gmail.com>
2025-11-16 03:06:12 +0800

    Modified by JunBin

--- vivaQ6.java
+++ vivaQ6.java
@@ -1,71 +1,56 @@
-package question6;
 import java.util.Scanner;
 
 public class vivaQ6 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-
+        System.out.print("Enter number of logs, T (1-100): ");
         int T=sc.nextInt();
-        String[] str=new String[T];
-
-        for(int i=0;i<T;i++){
-            str[i]=sc.next();
+        while ((T<1) || (T>100)){
+            System.out.print("T can only be 1 to 100. Please enter again: ");
+            T = sc.nextInt();
         }
 
         for(int i=0;i<T;i++){
-            boolean done=false;
-            while(done==false){
+            System.out.print("Enter the compressed string (1-50 characters): ");
+            String compressed = sc.next().toLowerCase();
+            int length=0;
+            while (compressed.length()<1 || compressed.length()>100){
+                System.out.print("The compressed string can only be 1-50 characters. Please enter again: ");
+                compressed = sc.next().toLowerCase();
+            }
+            
+            boolean inputValid=true;
+            if(Character.isDigit(compressed.charAt(0))){
+                inputValid=false;
+            }
 
-                int length=0;
-                boolean good=true;
-
-                if(str[i].isEmpty() || Character.isDigit(str[i].charAt(0))){
-                    good=false;
-                }
-
-                if(good){
-                    for(int j=0;j<str[i].length();j++){
-
-                        if(Character.isLetter(str[i].charAt(j))){
-                            length++;
-                        }
-                        else if(Character.isDigit(str[i].charAt(j))){
-
-                            if(str[i].charAt(j)=='0' || str[i].charAt(j)=='1'){
-                                good=false;
-                                break;
+            if(inputValid){
+                for(int j=0;j<compressed.length();j++){
+                    char currentLetter = compressed.charAt(j);
+                    if(Character.isLetter(currentLetter)){
+                        length++;
+                    }else if(Character.isDigit(currentLetter)){
+                        if(currentLetter=='0' || currentLetter=='1'){
+                            inputValid=false;
+                            break;
+                        }else if ((j+1)==compressed.length() || !(Character.isDigit(compressed.charAt(j+1)))){
+                            int digitValue = currentLetter-'0';
+                            for (int count=0; count<digitValue-1; count++){
+                                length++;
                             }
-
-                            if(j==0 || Character.isDigit(str[i].charAt(j-1))){
-                                good=false;
-                                break;
-                            }
-
-                            length+=(str[i].charAt(j)-'0')-1;
-                        }
-                        else{
-                            good=false;
+                        }else{
+                            inputValid=false;
                             break;
                         }
                     }
                 }
-
-                if(good&&length>200){
-                    str[i]=sc.next();
-                    continue;
-                }
-                if (str[i].length()>50){
-                    str[i]=sc.next();
-                    continue;
-                }
-
-                if(good && length<=200){
-                    System.out.println(length);
-                } 
-                else if(!good){
-                    System.out.println("Invalid Log");
-                }
-                done=true;
+            }
+            if(inputValid && length<=200){
+                System.out.println(length);
+            }else if(!inputValid){
+                System.out.println("Invalid Log");
+            }else if (length>200){
+                System.out.println("The decompressed string exceed 200 characters in length.");
             }
         }
     }
